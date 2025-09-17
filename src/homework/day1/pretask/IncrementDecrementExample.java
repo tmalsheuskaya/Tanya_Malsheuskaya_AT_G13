8c608605140ce58fc92a14d271873f4d4c73d50b
tmalsheuskaya <t.malsheuskaya@gmail.com>
2025-09-17 22:14:21 +0200

    Added pretask

---
+++ src/homework/day1/pretask/IncrementDecrementExample.java
@@ -0,0 +1,15 @@
+package pretask;
+
+public class IncrementDecrementExample {
+    public static void main(String[] args) {
+        int x = 3;
+
+        System.out.println(x++);
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x--);
+        System.out.println(x--);
+        System.out.println(--x);
+       // System.out.println(x);
+    }
+}
---
+++ src/homework/day1/pretask/MessageExample.java
@@ -0,0 +1,8 @@
+package pretask;
+
+public class MessageExample {
+    public static String text = "This is my amazing code";
+    public static void main(String [] args) {
+        System.out.println(text);
+    }
+}
