3799a845c5082f41c9aa6b41aa837bba3b136650
tmalsheuskaya <t.malsheuskaya@gmail.com>
2025-09-17 22:22:03 +0200

    Added bubbles

--- src/bubbles/Water.java
+++ src/bubbles/Water.java
@@ -1,4 +1,15 @@
 package bubbles;
 
-public class Water {
+public abstract class Water {
+    protected String color;
+    protected String transparency;
+    protected String smell;
+    protected double temperature;
+
+    public Water(String color, String transparency, String smell, double temperature) {
+        this.color = color;
+        this.transparency = transparency;
+        this.smell = smell;
+        this.temperature = temperature;
+    }
 }
