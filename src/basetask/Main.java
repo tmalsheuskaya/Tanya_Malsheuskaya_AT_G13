de4598b535bd68e940bb9cada20f7f1cf0fa1391
tmalsheuskaya <t.malsheuskaya@gmail.com>
2025-09-17 22:36:23 +0200

    Delete src/basetask directory

--- src/basetask/Main.java
+++
@@ -1,8 +0,0 @@
-package basetask;
-
-public class Main {
-    public static void main(String[] args) {
-        Pineapple ananas = new Pineapple("12 ", 9000);
-        ananas.printPineappleDetails();
-    }
-}
